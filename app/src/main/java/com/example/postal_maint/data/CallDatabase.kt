13b9b7cf42159170f7a328dbf5fdb7072aa22ba2
jy32m <jy32m0@gmail.com>
2023-06-23 18:07:53 -1000

    from LV

--- app/src/main/java/com/example/postal_maint/data/CallDatabase.kt
+++ app/src/main/java/com/example/postal_maint/data/CallDatabase.kt
@@ -1,12 +1,29 @@
 package com.example.postal_maint.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [CallEntity::class], version = 1, exportSchema = false)
-abstract class CallDatabase : RoomDatabase {
+abstract class CallDatabase : RoomDatabase() {
     abstract fun callDao(): CallDao
     companion object {
+        @Volatile
+        private var INSTANCE: CallDatabase? = null
 
+        fun getDatabase(context: Context): CallDatabase {
+            return INSTANCE ?: synchronized(this) {
+                Room
+                    .databaseBuilder(
+                        context,
+                        CallDatabase::class.java,
+                        "call_table"
+                    )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { INSTANCE = it }
+            }
+        }
     }
 }
